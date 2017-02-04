14096f7c370091720c823e614304a369adb4cc47
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-02-04 15:15:16 +0530

    runtime camera permission added
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/codingblocks/gdg_hunt/MainActivity.java
+++ app/src/main/java/com/codingblocks/gdg_hunt/MainActivity.java
@@ -1,6 +1,11 @@
 package com.codingblocks.gdg_hunt;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +14,19 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button b1,b2;
+    int permReqCode = 111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int perm = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        if(perm == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this , new String[]{Manifest.permission.CAMERA},permReqCode);
+        }
+
 
         b1= (Button) findViewById(R.id.button);
         b2= (Button) findViewById(R.id.button2);
